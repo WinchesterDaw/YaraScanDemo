bdddb2acd8b72e417771a0317160e4587793aa25
WinchesterDaw <2012935@mail.nankai.edu.cn>
2021-12-09 00:23:09 +0800

    updated

--- YaraScanDemo/YaraScanDemo.py
+++ YaraScanDemo/YaraScanDemo.py
@@ -1,8 +1,15 @@
-from PySide2.QtWidgets import QApplication,QMainWindow,QPushButton,QPlainTextEdit,QLineEdit
 import os
 import hashlib
 import csv
 import yara
+import argparse
+import shutil
+import sys
+num = 0
+names=[]
+
+lines=[]#白名单行
+
 
 def get_path(p,x):#获取文件绝对路径
     return os.path.join(p,x)
@@ -40,97 +47,71 @@ def get_md5(name):
     return hash_value
 
 
+def whitelist(x):
+    for line in lines:
+        if x==line:
+            return False
+    return True
+
+
 def get_files(paths,filenames):
-    try:
-      file_list=os.listdir(paths)
-      file_list=list(map(lambda x:get_path(paths,x),file_list))#找到所有文件的绝对路径
-      f_names=list(filter(os.path.isfile,file_list))#找到当前目录下的文件
-      names.extend(f_names)#存入names
-      d_names=list(filter(os.path.isdir,file_list))#找出当前目录下文件夹名
-      go=list(map(lambda x:get_files(get_path(paths,x),filenames),d_names))
-    except PermissionError:
-      print ("无法打开文件夹 "+paths)
-       
-#def get_csv(filenames):
-#    f = open('1.csv','w',encoding='utf-8e')
-#    csv_writer = csv.writer(f)
-#    csv_writer.writerow(["文件","md5","sha256"])
-#    ex =list( map(lambda x:csv_writer.writerow([x,get_md5(x),get_sha256(x)]) ,names))
-#    f.close()
-names=[];
+        try:
+            file_list = os.listdir(paths)
+            file_list = list(map(lambda x: get_path(paths, x), file_list))  # 找到所有文件的绝对路径
+            #file_list=list(filter(whitelist,file_list))
+            f_names = list(filter(os.path.isfile, file_list))  # 找到当前目录下的文件
+            names.extend(f_names)  # 存入names
+            d_names = list(filter(os.path.isdir, file_list))  # 找出当前目录下文件夹名
+            go = list(map(lambda x: get_files(get_path(paths, x), filenames), d_names))
+        except (PermissionError,FileNotFoundError):
+            pass
+
+
 def run(rulepath,path,names):
-   # path = 'D:/malware'#恶意软件目录，可更改
-   if(path=='None' or rulepath=="None"):
-         textEdit.setPlainText('文件路径为空')
-         textEdit.setPlainText('导入路径无效')
-         return 0
-   else: 
       my_file=get_files(path,[])
-      names=list(filter(lambda x:x.endswith(".exe"),names))
+      #names=list(filter(lambda x:x.endswith(".fas") or x.endswith(".lsp") or x.endswith(".bak") ,names))
       #rulepath ="D:/YaraRules"#yara规则目录，可更改
       yararule=get_rules(rulepath)#得到编译后的规则
       names=list(filter(lambda x:scan(yararule,x),names))#扫描names中的文件得到结果
-      for x in names:
-          textOut.insertPlainText(x)
+      with open('Log.txt', 'a') as f:
+          for name in names:
+              global num
+              temp = num
+              curname = name + str(temp) + ".bak"
+              try:
+                  os.rename(name, curname)
+                  shutil.copy(curname, "./isolation")
+              except  (FileNotFoundError,PermissionError):
+                   pass
+              f.write(curname + '\n')
+              num = num + 1
+def delete():
+    with open("Log.txt", 'r+') as f:
+        f.truncate(0)
 
-     #以上为功能函数
-rulepath='None'
-path='None'
-def pathIn():#获取用户输入的路径
-     global path
-     path = textEdit.text()
-     textEdit.setPlaceholderText('当前扫描文件路径为'+path)
-     textEdit.clear()
+rulepath=os.path.abspath('./rules')
+path='D:/abc'
+if __name__ =="__main__":
+    parser =argparse.ArgumentParser(description='yarScan')
+    parser.add_argument('-m', help='Path to scan for malware')
+    args = parser.parse_args()
+    if args.m:
+         if os.path.isfile(args.m):
+          sys.exit(0)
+         else:
+          path=args.m
+    #f=open('whitelist.txt')
+    #lines=f.readlines()
+    #f.close()
+    delete()
+    if not path == 'None':
+     try:
+      run(rulepath,path,names)
+     except (FileNotFoundError,PermissionError):
+         pass
+     print("success")
+    #dirpath=os.path.abspath("isolation")
 
-def rulepathIn():#获取用户输入的路径
-     global rulepath
-     rulepath = textEdit1.text()
-     textEdit1.setPlaceholderText('当前导入规则路径为'+rulepath)
-     textEdit1.clear()
-
-def button0_handle():#点击扫描按钮
-    global names
-    run(rulepath,path,names)
-
-def button1_handle():#点击生成csv按钮
-    global names
-    get_csv(names)
-app = QApplication([])
-
-
-window = QMainWindow()
-window.resize(500,400)
-window.move(300,310)
-window.setWindowTitle('恶意文件扫描')
-
-textEdit=QLineEdit(window)
-textEdit.setPlaceholderText('请输入文件路径')
-textEdit.move(10,25)
-textEdit.resize(300,50)
-textEdit.returnPressed.connect(pathIn)
-
-textEdit1=QLineEdit(window)
-textEdit1.setPlaceholderText('请输入导入规则路径')
-textEdit1.move(10,85)
-textEdit1.resize(300,50)
-textEdit1.returnPressed.connect(rulepathIn)
-
-
-textOut=QPlainTextEdit(window)
-textOut.setPlaceholderText('扫描结果将在此显示')
-textOut.move(10,200)
-textOut.resize(400,100)
-
-
-button0=QPushButton('扫描',window)#扫描开始
-button0.clicked.connect(button0_handle)
-button0.move(250,350)
-
-#button1=QPushButton('生成csv文件',window)#生成恶意文件的哈希值
-#button1.clicked.connect(button1_handle)
-#button1.move(350,350)
-
-
-
-window.show()
-app.exec_()
+ 
+            
+        
